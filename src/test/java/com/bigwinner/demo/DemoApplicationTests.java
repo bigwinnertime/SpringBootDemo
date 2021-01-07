3204638fc872c471c4e40a9b8bbcc18ba50caca0
liushili <stearnsliu@gmail.com>
2021-01-07 10:53:42 +0800

    init springboot demo

---
+++ src/test/java/com/bigwinner/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigwinner.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
